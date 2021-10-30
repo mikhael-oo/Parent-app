65d486a0a1bfc7e678a25b6ef23f04a93e60a671
Parham Khadem <parham.snp@gmail.com>
2021-10-29 18:49:01 -0700

    Created model of Coin

---
+++ app/src/main/java/Model/Coin.java
@@ -0,0 +1,30 @@
+package Model;
+
+/**
+ * Coin class, represents a fair coin with head and tail
+ * It will be used in parent support app to be used by kids for various purposes
+ * Singleton model class
+ */
+
+public class Coin {
+
+    private static Coin instance = null;
+
+    private Coin()  {}
+
+    public static Coin getCoinInstance()    {
+
+        if(instance == null)   {
+            instance = new Coin();
+        }
+        return instance;
+    }
+
+
+    // return true for head, false for tail
+    public boolean toss()   {
+
+        int toss = (int) (Math.random() + 0.5);
+        return toss == 1;
+    }
+}
