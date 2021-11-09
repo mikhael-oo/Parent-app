52f636bb028e6045d123426335ae717a844d033f
a_shokar <arjun.shokar@gmail.com>
2021-11-08 22:20:38 -0800

    changed ui of text and cleaned up code

--- app/src/main/java/com/example/parentapp/AddKidActivity.java
+++ app/src/main/java/com/example/parentapp/AddKidActivity.java
@@ -17,6 +17,10 @@ import androidx.appcompat.widget.Toolbar;
 import com.example.parentapp.models.Kid;
 import com.example.parentapp.models.KidManager;
 
+/*
+adds child to the the list when asked for its name
+adds to the array list
+ */
 public class AddKidActivity extends AppCompatActivity {
 
     KidManager manager;
